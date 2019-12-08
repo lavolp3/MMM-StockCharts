c73130f4fc1e5bd6919bf485afd7aafe031fed57
Dirk <dirk.kovert@gmail.com>
2019-12-09 00:39:55 +0100

    first commit

---
+++ MMM-StockCharts.js
@@ -0,0 +1,220 @@
+
+
+Module.register("MMM-StockCharts", {
+  defaults: {
+    apiKey : "",
+    timeFormat: "DD-MM HH:mm",
+    symbols : ["MNKD"],
+    alias: ["APPLE", "", "SAMSUNG Electronics"],
+    tickerDuration: 60,
+    chartDays: 60,
+    decimals : 4,
+    candleSticks: false,
+    coloredCandles: true,
+    showMA: "SMA",           //can be "SMA" or "EMA"
+    maPeriod: "200",         //can be any numerical value. 200 is common in stock analysis
+    premiumAccount: false,   // To change poolInterval, set this to true - Only For Premium Account
+  },
+
+  getStyles: function() {
+    return ["MMM-StockCharts.css"];
+  },
+
+  start: function() {
+    this.sendSocketNotification("GET_STOCKDATA", this.config);
+    this.stocks = {};
+    this.isStarted = false;
+  },
+
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.id = "stockChart";
+    return wrapper;
+  },
+
+  getStockName: function(symbol) {
+    var stockAlias = symbol;
+    var i = this.config.symbols.indexOf(symbol);
+    if (this.config.symbols.length == this.config.alias.length) {
+      stockAlias = (this.config.alias[i]) ? this.config.alias[i] : stockAlias;
+    }
+    return stockAlias;
+  },
+
+
+  prepareChart: function() {
+    var wrapper = document.getElementById("AVSTOCK");
+    wrapper.innerHTML = "";
+
+    var stock = document.createElement("div");
+    stock.innerHTML = "";
+    stock.id = "AVSTOCK_SERIES";
+    stock.className = "stock";
+
+    var symbol = document.createElement("div");
+    symbol.className = "symbol";
+    symbol.innerHTML = "Loading...";
+    symbol.id = "symbol_series";
+
+    var price = document.createElement("div");
+    price.className = "price";
+    price.innerHTML = "---";
+    price.id = "price_series";
+
+    var change = document.createElement("div");
+    change.className = "change";
+    change.innerHTML = "---";
+    change.id = "change_series";
+
+    var anchor = document.createElement("div");
+    anchor.className = "anchor";
+
+    anchor.appendChild(price);
+    anchor.appendChild(change);
+
+    stock.appendChild(symbol);
+    stock.appendChild(anchor);
+    wrapper.appendChild(stock);
+
+    var cvs = document.createElement("canvas");
+    cvs.id = "stockchart_canvas";
+    wrapper.appendChild(cvs);
+
+    var tl = document.createElement("div");
+    tl.className = "tagline";
+    tl.id = "AVSTOCK_TAGLINE";
+    tl.innerHTML = "Last updated : ";
+    wrapper.appendChild(tl)
+
+    cvs = document.getElementById("AVSTOCK_CANVAS");
+    cvs.width = cvs.clientWidth;
+    cvs.height = cvs.clientHeight;
+  },
+
+  socketNotificationReceived: function(noti, payload) {
+    if (noti == "UPDATE_QUOTE") {
+      if (payload.hasOwnProperty('symbol')) {
+        this.stocks[payload.symbol] = payload
+        this.updateQuote(payload)
+      }
+    }
+    if (noti == "UPDATE_SERIES") {
+      this.updateSeries(payload)
+    }
+    if (noti == "UPDATE_MA") {
+      this.drawMA(payload)
+    }
+  },
+
+  drawSeries: function(series) {
+    var symbol = ""
+    var co = []
+    var changeV = 0
+    var lastPrice = 0
+    var requestTime = ""
+
+    var cvs = document.getElementById("AVSTOCK_CANVAS")
+    var ctx = cvs.getContext("2d")
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+    //determine max, min etc. for graph size
+    for(var i in series) {
+      var s = series[i];
+      co[i] = s.close;
+      if (i == 0) {
+        max = s.close;
+        min = s.close;
+        symbol = s.symbol;
+        ud = s.candle;
+        lastPrice = s.close;
+        requestTime = s.requestTime;
+      } else if (!this.config.candleSticks) {
+        if (s.close > max) {
+          max = s.close;
+        }
+        if (s.close < min) {
+          min = s.close;
+        }
+      } else {
+        if (s.high > max) {
+          max = s.high;
+        }
+        if (s.low < min) {
+          min = s.low;
+        }
+      }
+      if (i == 1) {
+          changeV = Math.round((lastPrice - s.close) * 10000) / 10000;
+      }
+    }
+
+    ctx.beginPath();   //draw line or candle stick chart
+    var xs = Math.round(((ctx.canvas.width)-10) / series.length);
+    var x = 5;
+    var y = 0;
+    var y2 = 0;
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.lineWidth = 3;
+    if (!this.config.candleSticks) {
+      ctx.beginPath();
+      for (i = 0; i < series.length; i++) {
+        var t = series[i];
+        var c = t.close;
+        y = ctx.canvas.height - (((c - min) / (max - min)) * ctx.canvas.height);
+        ctx.lineTo(x, y);
+        x += xs;
+      }
+      ctx.stroke();
+    } else {
+      for (i = 0; i < series.length; i++) {
+        ctx.lineWidth = 1;
+        var t = series[i];
+        y = ctx.canvas.height - (((t.high - min) / (max - min)) * ctx.canvas.height);
+        y2 = ctx.canvas.height - (((t.low - min) / (max - min)) * ctx.canvas.height);
+        ctx.beginPath();    //drawing the candlestick from t.high to t.low
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y2);
+        ctx.stroke();
+        ctx.beginPath();  //drawing the candle from t.open to t.close
+        var rectMinY = ctx.canvas.height - (((Math.min (t.close, t.open) - min) / (max - min)) * ctx.canvas.height);
+        var rectMaxY = ctx.canvas.height - (((Math.max (t.close, t.open) - min) / (max - min)) * ctx.canvas.height);
+        if (this.config.coloredCandles) {
+          ctx.fillStyle = ((t.close < t.open) ? "red" : "green");
+        } else {
+          ctx.fillStyle = ((t.close < t.open) ? "black" : "white");
+        }
+        ctx.fillRect(x-Math.round(xs/2)+2, rectMinY, xs-4, rectMaxY-rectMinY);      //filled black or white (or colored) candle written above the candlestick
+        ctx.strokeRect(x-Math.round(xs/2)+2, rectMinY, xs-4, rectMaxY-rectMinY);   //white border
+        x += xs;
+      }
+    }
+
+
+    if (cfg.showMA) {
+      ctx.beginPath();
+      ctx.strokeStyle = "#11A6E2";
+      for (i = 0; i < series.length; i++) {
+        var t = series[i].ma;
+        y = ctx.canvas.height - (((t - min) / (max - min)) * ctx.canvas.height);
+        ctx.lineTo(x, y);
+        x += xs;
+      }
+      ctx.stroke();
+    }
+
+    var stock = document.getElementById("symbol_series");
+    stock.innerHTML = this.getStockName(symbol);
+    var price = document.getElementById("price_series");
+    price.innerHTML = ""
+    var change = document.getElementById("change_series");
+    change.innerHTML = changeV;
+
+    var tr = document.getElementById("AVSTOCK_SERIES");
+    tr.className = "animated stock " + ud;
+    var tl = document.getElementById("AVSTOCK_TAGLINE");
+    tl.innerHTML = "Last updated: " + requestTime;
+    setTimeout(()=>{
+      tr.className = "stock " + ud;
+    }, 1500);
+  },
+})
---
+++ node_helper.js
@@ -0,0 +1,101 @@
+/* jshint esversion: 6 */
+
+const request = require('request');
+const moment = require('moment');
+
+var NodeHelper = require("node_helper");
+
+module.exports = NodeHelper.create({
+  start: function() {
+    this.config = null;
+    this.stockData = {};
+  },
+
+  socketNotificationReceived: function(noti, payload) {
+    if (noti == "GET_STOCKDATA") {
+      cfg = payload.config;
+      var interval = parseInt((24 * 60 * 60 * 1000) / 490)          //500 calls allowed in 24 hours
+      this.log(Math.Round(interval/1000));
+      var counter = [0,0];
+      var _this = this;
+      setInterval(() => {
+        _this.callAPI(cfg, cfg.stocks[counter[0]], cfg.ma[counter[1]]);
+        counter[1] += 1;
+        if (counter[1] == cfg.ma.length) {
+          counter[0] += 1;
+          counter[1] = 0;
+        }
+        if (counter[0] == cfg.stocks.length) {
+          counter[0] = 0;
+        }
+        _this.log("Counter: "+counter);
+      }, interval)
+    }
+  },
+
+  callAPI: function(cfg, symbol, func) {
+    var url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=";
+    url += symbol + "&apikey=" + cfg.apiKey;
+
+    request(url, (error, response, body)=>{
+      //this.log("[AVSTOCK] API is called - ", symbol)
+      var data = null;
+      if (error) {
+        this.log("API Error: ", error);
+        return;
+      }
+      data = JSON.parse(body);
+      if (data.hasOwnProperty("Note")) {
+        this.log("[AVSTOCK] Error: API Call limit exceeded.");
+      }
+      if (data.hasOwnProperty("Error Message")) {
+        this.log("[AVSTOCK] Error:", data["Error Message"]);
+      }
+      if (data["Time Series (Daily)"]) {
+        //console.log("[AVSTOCK] Response is parsed - ", symbol)
+        this.prepareData(data["Time Series (Daily)"]);
+      }
+    })
+  },
+
+
+  prepareData: function(series) {
+    // split the data set into ohlc and volume
+    var ohlc = [],
+      volume = [],
+      dataLength = data.length,
+      // set the allowed units for data grouping
+      groupingUnits = [[
+        'week',                         // unit name
+        [1]                             // allowed multiples
+      ], [
+        'month',
+        [1, 2, 3, 4, 6]
+      ]];
+    var keys = Object.keys(series);
+    var dayLimit = (this.config.chartDays > 90) ? 90 : this.config.chartDays;
+    keys = keys.slice(0, dayLimit).sort();
+
+    for (var k in keys) {
+      index = keys[k];
+      ohlc.push([
+        moment(k, "YYYY-MM-DD").format("x"), // the date
+        parseFloat(series[index]["1. open"]), // open
+        parseFloat(series[index]["2. open"]), // high
+        parseFloat(series[index]["3. open"]), // low
+        parseFloat(series[index]["4. open"]) // close
+      ]);
+
+      volume.push([
+        moment(k, "YYYY-MM-DD").format("x"), // the date
+        parseInt(series[index]["5. open"]) // the volume
+      ]);
+    }
+  },
+
+  log: function (msg) {
+    if (this.config && this.config.debug) {
+      console.log(this.name + ":", JSON.stringify(msg));
+    }
+  },
+})
